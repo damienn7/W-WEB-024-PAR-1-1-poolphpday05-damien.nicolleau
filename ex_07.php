e71354939b8b0d9d60167fa928aa71cd6ff06a75
damienn7 <118727505+damienn7@users.noreply.github.com>
2022-11-28 16:52:13 +0100

    boucle foreach de l'exercice 12 ajoutée

--- ex_07.php
+++ ex_07.php
@@ -1,15 +1,18 @@
 <?php
+session_start();
     /*
     $_SESSION["name"]="toto";
     $_SESSION["email"]="titi";
     $_SESSION["pass"]="tata";
     */
 function my_reset_session()
-{
+{   
+
+
     foreach($_SESSION as $key => $value)
     {
         $key = NULL;
-        $value=NULL;
+        $value = NULL;
         //echo $value;
     }
     //var_dump($_SESSION);
